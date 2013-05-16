714d653336b5e9eb3a141689c1437ca00de061e6
Michael Contento <michaelcontento@gmail.com>
2013-05-16 04:35:00 +0200

    add ios backend for ui::Alert

---
+++ avalon/platform/ios/ui/AlertNative.mm
@@ -0,0 +1,64 @@
+#include <avalon/ui/Alert.h>
+
+#include <avalon/ui/AlertDelegate.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#include "cocos2d.h"
+
+@interface AlertViewProxy : NSObject<UIAlertViewDelegate>
+{
+    @public avalon::ui::AlertDelegate* delegate;
+}
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex;
+@end
+
+@implementation AlertViewProxy
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (!delegate) {
+        return;
+    }
+
+    NSString* buttonTitle = [alertView buttonTitleAtIndex:buttonIndex];
+    delegate->onAlertButtonClick(buttonIndex, [buttonTitle UTF8String]);
+
+    [self release];
+    [alertView release];
+}
+@end
+
+namespace avalon {
+namespace ui {
+
+void showAlert(const std::string& title, const std::string& message, const ButtonList& buttons, AlertDelegate* delegate)
+{
+    AlertViewProxy* proxy = [[[[AlertViewProxy alloc] init] autorelease] retain];
+    proxy->delegate = delegate;
+
+    NSString* cancelTitle = nil;
+    if (buttons.size() > 0) {
+        cancelTitle = [NSString stringWithUTF8String:buttons.begin()->second.c_str()];
+    }
+
+    UIAlertView* view = [[[[UIAlertView alloc] initWithTitle:[NSString stringWithUTF8String:title.c_str()]
+                                                   message:[NSString stringWithUTF8String:message.c_str()]
+                                                  delegate:proxy
+                                         cancelButtonTitle:cancelTitle
+                                         otherButtonTitles:nil] autorelease] retain];
+
+
+    bool firstSkipped = false;
+    for (auto& button : buttons) {
+        if (!firstSkipped) {
+            firstSkipped = true;
+            continue;
+        }
+
+        [view addButtonWithTitle:[NSString stringWithUTF8String:button.second.c_str()]];
+    }
+
+    [view show];
+}
+
+} // namespace ui
+} // namespace avalon
