1d37bfdf1b6fcf42ae871f7890215e3ee2328aef
Michael Contento <michaelcontento@gmail.com>
2013-05-28 02:28:37 +0200

    add samsung payment backend

---
+++ avalon/platform/android-samsung/payment/java/src/com/avalon/payment/Backend.java
@@ -0,0 +1,89 @@
+package com.avalon.payment;
+
+import java.util.List;
+import java.util.ArrayList;
+import android.content.Intent;
+
+import com.avalon.payment.PurchasingObserver;
+
+public class Backend
+{
+    private static PurchasingObserver mPurchaseObserver = null;
+    private static List<String> pendingItemData = new ArrayList<String>();
+    private static int itemDataReturned = 0;
+
+    /**
+     *
+     * Methods called from the C++ side
+     *
+     */
+
+    public static boolean isInitialized()
+    {
+        return (mPurchaseObserver != null);
+    }
+
+    public static void initialize()
+    {
+        if (!isInitialized()) {
+            mPurchaseObserver = new PurchasingObserver();
+        }
+    }
+
+    public static void shutdown()
+    {
+    }
+
+    public static void purchase(String productId, boolean isConsumable)
+    {
+        mPurchaseObserver.purchase(productId, isConsumable);
+    }
+
+    public static boolean isPurchaseReady()
+    {
+        return isInitialized() && itemDataReturned > 0;
+    }
+
+    public static void addItemDataRequest(String productId)
+    {
+        pendingItemData.add(productId);
+    }
+
+    public static void startItemDataRequest()
+    {
+        mPurchaseObserver.startItemDataRequest(pendingItemData);
+    }
+
+    public static void delegateOnItemData(String productId, String name, String desc, String priceStr, float price)
+    {
+        pendingItemData.clear();
+        ++itemDataReturned;
+        onItemData(productId, name, desc, priceStr, price);
+    }
+
+    /**
+     *
+     * Methods to call back into the C++ side
+     *
+     */
+
+    public static native void delegateOnServiceStarted();
+    public static native void delegateOnPurchaseSucceed(String productId);
+    public static native void delegateOnPurchaseFail();
+    public static native void delegateOnTransactionStart();
+    public static native void delegateOnTransactionEnd();
+    public static native void onItemData(String productId, String name, String desc, String priceStr, float price);
+    public static native void onInitialized();
+
+    /**
+     *
+     * Helper methods to integrate this lib into your app. Should be called in
+     * your main activity - please read the docs provided with this library!
+     *
+     */
+
+    public static void setItemGroupId(final String itemGroupId)
+    {
+        PurchasingObserver.itemGroupId = itemGroupId;
+    }
+}
