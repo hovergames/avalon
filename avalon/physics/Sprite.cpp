e0bec5bcf142b6d562e758d469c54d5406d2f64c
Michael Contento <michaelcontento@gmail.com>
2013-11-01 14:11:31 +0100

    physics::Sprite with PES and CCBLoader support

--- avalon/physics/Sprite.cpp
+++ avalon/physics/Sprite.cpp
@@ -2,6 +2,7 @@
 
 #include <avalon/utils/tiled.h>
 #include <avalon/physics/Box2dContainer.h>
+#include <avalon/physics/vendors/GB2ShapeCache.h>
 
 namespace avalon {
 namespace physics {
@@ -32,37 +33,136 @@ b2Body& Sprite::getBody()
 
 void Sprite::onConfiguration(const avalon::io::CCBLoader::Configuration& config)
 {
+    if (config.settings.count("pes.file"))
+    {
+        if (!config.settings.count("pes.shape")) throw std::invalid_argument("pes.shape not defined!");
+        addFixtureToBodyFromPESFile(boost::any_cast<std::string>(config.settings.at("pes.file")), boost::any_cast<std::string>(config.settings.at("pes.shape")), *config.box2dContainer);
+    } else {
+        createBody(*config.box2dContainer);
+        createDefaultShape();
+    }
+
+    float x = getPositionX();
+    float y = getPositionY();
+    setPosition({x, y});
+
+    if (config.settings.count("friction"))
+        getBody().GetFixtureList()->SetFriction(boost::any_cast<float>(config.settings.at("friction")));
+
+    if (config.settings.count("density"))
+        getBody().GetFixtureList()->SetDensity(boost::any_cast<float>(config.settings.at("density")));
+
+    if (config.settings.count("restitution"))
+        getBody().GetFixtureList()->SetRestitution(boost::any_cast<float>(config.settings.at("restitution")));
+
+    if (config.settings.count("bodytype"))
+        getBody().SetType(getBox2dBodyType(boost::any_cast<std::string>(config.settings.at("bodytype"))));
 }
 
 void Sprite::onConfiguration(const avalon::io::TiledMapLoader::Configuration& config)
 {
+    if (config.settings.count("pes.file"))
+    {
+        if (!config.settings.count("pes.shape")) throw std::invalid_argument("pes.shape not defined!");
+        addFixtureToBodyFromPESFile(config.settings.at("pes.file"), config.settings.at("pes.shape"), *config.box2dContainer);
+    } else {
+        createBody(*config.box2dContainer);
+        createDefaultShape();
+    }
+
     float x = std::stoi(config.settings.at("x"));
     float y = std::stoi(config.settings.at("y"));
+    auto pos = avalon::utils::tiled::getPositionFromCoord(config.map, x, y);
+    setPosition(pos);
+
+    if (config.settings.count("friction"))
+        getBody().GetFixtureList()->SetFriction(std::stof(config.settings.at("friction")));
+
+    if (config.settings.count("density"))
+        getBody().GetFixtureList()->SetDensity(std::stof(config.settings.at("density")));
+
+    if (config.settings.count("restitution"))
+        getBody().GetFixtureList()->SetRestitution(std::stof(config.settings.at("restitution")));
+
+    if (config.settings.count("bodytype"))
+        getBody().SetType(getBox2dBodyType(config.settings.at("bodytype")));
+}
+
+b2BodyType Sprite::getBox2dBodyType(const std::string& type)
+{
+    if (type == "static") {
+        return b2_staticBody;
+    } else if (type == "dynamic") {
+        return b2_dynamicBody;
+    } else if (type == "kinematic") {
+        return b2_kinematicBody;
+    } else {
+        throw new std::invalid_argument("Unknown box2d type");
+    }
+}
+
+void Sprite::createDefaultShape()
+{
     float w = getContentSize().width;
     float h = getContentSize().height;
 
-    auto pos = avalon::utils::tiled::getPositionFromCoord(config.map, x, y);
-    cocos2d::Sprite::setPosition(pos);
-    pos.x -= w * getAnchorPoint().x;
-    pos.y -= h * getAnchorPoint().y;
-
-    // create the body
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_staticBody;
-    bodyDef.position.Set((pos.x + (w / 2.0f)) / config.box2dContainer->pixelsInMeter,
-                         (pos.y + (h / 2.0f)) / config.box2dContainer->pixelsInMeter);
-    auto body = config.box2dContainer->createBody(bodyDef, *this);
-    box2dContainer = config.box2dContainer;
-
-    // define the shape
     b2PolygonShape shape;
-    shape.SetAsBox((w / config.box2dContainer->pixelsInMeter) * 0.5f,
-                   (h / config.box2dContainer->pixelsInMeter) * 0.5f);
+    shape.SetAsBox((w / box2dContainer->pixelsInMeter) * 0.5f,
+                   (h / box2dContainer->pixelsInMeter) * 0.5f);
 
-    // create the fixture
     b2FixtureDef fixtureDef;
     fixtureDef.shape = &shape;
-    body->CreateFixture(&fixtureDef);
+    getBody().CreateFixture(&fixtureDef);
+}
+
+void Sprite::createBody(Box2dContainer& box2dContainer)
+{
+    this->box2dContainer = &box2dContainer;
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    body = box2dContainer.createBody(bodyDef, *this);
+}
+
+void Sprite::addFixtureToBodyFromPESFile(std::string pesFileName, std::string shapeName, Box2dContainer& box2DContainer)
+{
+    if (!hasBody())
+        createBody(box2DContainer);
+
+    cocos2d::GB2ShapeCache::sharedGB2ShapeCache()->addShapesWithFile(pesFileName);
+    cocos2d::GB2ShapeCache::sharedGB2ShapeCache()->addFixturesToBody(&getBody(), shapeName);
+    setAnchorPoint(cocos2d::GB2ShapeCache::sharedGB2ShapeCache()->anchorPointForShape(shapeName));
+}
+
+bool Sprite::init(void)
+{
+    if (!cocos2d::Sprite::init())
+        return false;
+    
+    scheduleUpdate();
+    return true;
+}
+
+void Sprite::update(float delta)
+{
+    cocos2d::Sprite::update(delta);
+
+    if (hasBody()) {
+        float px = body->GetPosition().x * box2dContainer->pixelsInMeter;
+        float py = body->GetPosition().y * box2dContainer->pixelsInMeter;
+        float rotation = body->GetAngle();
+
+        cocos2d::Sprite::setPosition({px, py});
+        cocos2d::Sprite::setRotation(-CC_RADIANS_TO_DEGREES(rotation));
+    }
+}
+
+void Sprite::setPosition(const cocos2d::Point& pos)
+{
+    cocos2d::Sprite::setPosition(pos);
+
+    if (hasBody())
+        getBody().SetTransform(b2Vec2(pos.x / box2dContainer->pixelsInMeter, pos.y / box2dContainer->pixelsInMeter), getBody().GetTransform().q.GetAngle());
 }
 
 } // namespace physics
--- avalon/physics/Sprite.h
+++ avalon/physics/Sprite.h
@@ -2,10 +2,10 @@
 #define AVALON_PHYSICS_SPRITE_H
 
 #include "cocos2d.h"
+#include <Box2D/Box2D.h>
 #include <avalon/io/CCBLoader.h>
 #include <avalon/io/TiledMapLoader.h>
 
-class b2Body;
 namespace avalon { namespace physics { class Box2dContainer; } }
 
 namespace avalon {
@@ -17,13 +17,25 @@ private:
     b2Body* body = nullptr;
     avalon::physics::Box2dContainer* box2dContainer = nullptr;
 
+    void createBody(Box2dContainer& box2DContainer);
+    void createDefaultShape();
+    b2BodyType getBox2dBodyType(const std::string& type);
+
 public:
+    CREATE_FUNC(Sprite);
+    
     ~Sprite();
     void onConfiguration(const avalon::io::CCBLoader::Configuration& config);
     void onConfiguration(const avalon::io::TiledMapLoader::Configuration& config);
 
+    void addFixtureToBodyFromPESFile(std::string pesFileName, std::string shapeName, Box2dContainer& box2DContainer);
+
     bool hasBody() const;
     b2Body& getBody();
+
+    virtual bool init(void) override;
+    virtual void update(float delta) override;
+    virtual void setPosition(const cocos2d::Point& pos) override;
 };
 
 } // namespace physics
