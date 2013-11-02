ee9beeca81bddbc1b29f85c47e5e962244a369e4
Jochen Heizmann <info@intermediaware.com>
2013-11-02 04:33:03 +0100

    removed unused functions

--- avalon/physics/loadercallbacks.h
+++ avalon/physics/loadercallbacks.h
@@ -11,8 +11,6 @@ namespace loadercallbacks {
 avalon::io::TiledMapLoader::Callback createShapes(int filterCategory = 0, bool isSensor = false);
 
 std::shared_ptr<b2PolygonShape> initRectangleShape(float width, float height, float pixelsInMeter);
-std::shared_ptr<b2PolygonShape> initPolygonShape(int width, int height, float pixelsInMeter);
-std::shared_ptr<b2CircleShape> initCircleShape();
 std::shared_ptr<b2ChainShape> initChainShape(std::list<cocos2d::Point> points, float pixelsInMeter, bool loop = false);
 std::shared_ptr<b2EdgeShape> initEdgeShape(cocos2d::Point p1, cocos2d::Point p2, float pixelsInMeter);
 
