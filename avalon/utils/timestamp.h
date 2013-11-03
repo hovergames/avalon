ca91410db30fb93098f4c18f7d038e1b272add97
Jochen Heizmann <info@intermediaware.com>
2013-11-03 21:23:12 +0100

    getUnixtimestamp

---
+++ avalon/utils/timestamp.h
@@ -0,0 +1,14 @@
+#ifndef AVALON_UTILS_TIMESTAMP_H
+#define AVALON_UTILS_TIMESTAMP_H
+
+#include "cocos2d.h"
+
+namespace avalon {
+namespace utils {
+
+double getUnixTimestamp();
+
+} // namespace utils
+} // namespace avalon
+
+#endif /* AVALON_UTILS_TIMESTAMP_H */
