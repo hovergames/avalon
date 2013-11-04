e3260dd665411a69ebfaf560b2a9ccd72089db2c
Michael Contento <michaelcontento@gmail.com>
2013-11-04 21:54:28 +0100

    add ios::utils::getStringFromFile

---
+++ avalon/io/utils.cpp
@@ -0,0 +1,24 @@
+#include <avalon/io/utils.h>
+
+#include "cocos2d.h"
+
+namespace avalon {
+namespace io {
+namespace utils {
+
+std::string getStringFromFile(const std::string& file)
+{
+    unsigned long size = 0;
+    unsigned char* data = cocos2d::FileUtils::getInstance()->getFileData(file.c_str(), "r", &size);
+    if (size == 0) {
+        throw new std::invalid_argument("Unable to load from file");
+    }
+
+    std::string result(reinterpret_cast<const char*>(data));
+    delete[] data;
+    return result;
+}
+
+} // namespace utils
+} // namespace io
+} // namespace avalon
---
+++ avalon/io/utils.h
@@ -0,0 +1,16 @@
+#ifndef AVALON_IO_UTILS_H
+#define AVALON_IO_UTILS_H
+
+#include <string>
+
+namespace avalon {
+namespace io {
+namespace utils {
+
+std::string getStringFromFile(const std::string& file);
+
+} // namespace utils
+} // namespace io
+} // namespace avalon
+
+#endif /* AVALON_IO_UTILS_H */
