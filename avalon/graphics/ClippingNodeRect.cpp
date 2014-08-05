c24ee6a72950ae7ca6af7d02aa0dd1099ea9e93a
Jochen Heizmann <info@intermediaware.com>
2014-08-05 15:06:14 +0200

    update to cocos2d-x 3 (removed deprecated stuff)

--- avalon/graphics/ClippingNodeRect.cpp
+++ avalon/graphics/ClippingNodeRect.cpp
@@ -5,9 +5,9 @@ namespace graphics {
 
 void ClippingNodeRect::visit(cocos2d::Renderer *renderer, const cocos2d::Mat4& parentTransform, uint32_t parentFlags)
 {
-    cocos2d::kmGLPushMatrix();
-    glEnable(GL_SCISSOR_TEST);
+    cocos2d::Director::getInstance()->pushMatrix(cocos2d::MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
 
+    glEnable(GL_SCISSOR_TEST);
 
     auto glView = cocos2d::Director::getInstance()->getOpenGLView();
     glView->setScissorInPoints(
@@ -17,7 +17,8 @@ void ClippingNodeRect::visit(cocos2d::Renderer *renderer, const cocos2d::Mat4& p
     Node::visit();
 
     glDisable(GL_SCISSOR_TEST);
-    cocos2d::kmGLPopMatrix();
+
+    cocos2d::Director::getInstance()->popMatrix(cocos2d::MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
 }
 
 } // graphics
